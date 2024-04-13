7a08192df0bca7af5ee33100b632b4026c7573ff
Yamenko <yamenko@yandex.ru>
2024-04-13 23:51:48 +0300

    Добавьте файлы проекта.

---
+++ helper.h
@@ -0,0 +1,7 @@
+#pragma once
+
+
+int SqrSum(int a, int b)
+{
+	return (a + b) * (a + b);
+}
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "helper.h"
+
+int main()
+{
+	std::cout << SqrSum(10, 15);
+
+	return 0;
+}
