5835731a3562430967cb0366eccecd02c6f32ac1
Yamenko <yamenko@yandex.ru>
2024-04-14 00:12:04 +0300

    14.4 test

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
+#include <string>
 
 #include "helper.h"
 
 int main()
 {
+	// test 13.4
 	std::cout << SqrSum(10, 15);
 
 
+	// test 14.4
+	std::string name("It is test string.");
+
+	std::cout << "String: " << name << std::endl;
+	std::cout << "Length: " << name.size() << std::endl;
+	std::cout << "First char: " << *name.begin() << std::endl;
+	std::cout << "Last  char: " << *(name.end() - 1) << std::endl;
 
 	return 0;
 }
