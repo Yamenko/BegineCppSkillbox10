1f743a6ddd35a2065eb41ac36605004151562957
Yamenko <yamenko@yandex.ru>
2024-04-14 16:17:11 +0300

    test 16.5

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <string>
+#include <time.h>
 
 #include "helper.h"
+
+
 
 int main()
 {
@@ -21,7 +24,7 @@ int main()
 	std::cout << "-------------------------" << std::endl << std::endl;
 
 	// test 15.4
-	std::cout << "Test1.4: " << std::endl;
+	std::cout << "Test 15.4: " << std::endl;
 	const int n = 10;
 	std::cout << "Local loop: ";
 	for (int i = 0; i <= n; ++i)
@@ -43,5 +46,38 @@ int main()
 	std::cout << "-------------------------" << std::endl << std::endl;
 
 
+	// test 16.5
+	std::cout << "Test 16.5: " << std::endl;
+	const int m = 5;
+	int arr[m][m];
+	for (int i = 0; i < m; ++i){
+		for (int j = 0; j < m; ++j){
+			arr[i][j] = i + j;
+		}
+	}
+	for (int i = 0; i < m; ++i) {
+		for (int j = 0; j < m; ++j) {
+			std::cout << arr[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+
+	struct tm buf;
+	time_t t = time(NULL);
+	localtime_s(&buf, &t);
+
+	int index = buf.tm_mday % m;
+	int sum = 0;
+
+	for (int i = 0; i < m; ++i){
+		sum += arr[index][i];
+	}
+	std::cout << "data: " << buf.tm_mday << std::endl;
+	std::cout << "arr size: " << m << std::endl;
+	std::cout << "Index row: " << index << std::endl;
+	std::cout << "SUM: " << sum << std::endl;
+
+
+	std::cout << "-------------------------" << std::endl << std::endl;
 	return 0;
 }
