fe51910557c45710b19940bb916c5ed5245ca792
Yamenko <yamenko@yandex.ru>
2024-04-14 13:14:00 +0300

    finish test 15.4

--- helper.h
+++ helper.h
@@ -5,3 +5,29 @@ int SqrSum(int a, int b)
 {
 	return (a + b) * (a + b);
 }
+
+void PrintNumber(bool print_is_even, int n)
+{
+	for (int i = 0; i <= n; ++i)
+	{
+		if ((i%2) == 0 )
+		{
+			if (print_is_even) { std::cout << i << " "; }
+		}
+		else
+		{
+			if (!print_is_even) { std::cout << i << " "; }
+		}
+	}
+};
+
+void ModeratePrintNumber(bool print_is_even, int n)
+{
+	int i = 0;
+	if (!print_is_even) { i = 1; }
+
+	for (; i <= n; i+=2)
+	{
+		std::cout << i << " ";
+	}
+};
--- main.cpp
+++ main.cpp
@@ -6,16 +6,42 @@
 int main()
 {
 	// test 13.4
-	std::cout << SqrSum(10, 15);
-
+	std::cout << "Test13.4: " << std::endl;
+	std::cout << SqrSum(10, 15) << std::endl;
+	std::cout << "-------------------------" << std::endl << std::endl;
 
 	// test 14.4
 	std::string name("It is test string.");
 
+	std::cout << "Test14.4: " << std::endl;
 	std::cout << "String: " << name << std::endl;
 	std::cout << "Length: " << name.size() << std::endl;
 	std::cout << "First char: " << *name.begin() << std::endl;
 	std::cout << "Last  char: " << *(name.end() - 1) << std::endl;
+	std::cout << "-------------------------" << std::endl << std::endl;
+
+	// test 15.4
+	std::cout << "Test1.4: " << std::endl;
+	const int n = 10;
+	std::cout << "Local loop: ";
+	for (int i = 0; i <= n; ++i)
+	{
+		if ((i % 2) == 0) {
+			std::cout << i << " ";
+		}
+	}
+	std::cout << std::endl;
+
+	std::cout << "Use function loop: ";
+	PrintNumber(true, 6);
+	std::cout << std::endl;
+
+	std::cout << "Use moderate function loop: ";
+	ModeratePrintNumber(false, 6);
+	std::cout << std::endl;
+
+	std::cout << "-------------------------" << std::endl << std::endl;
+
 
 	return 0;
 }
